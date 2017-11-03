78662f511079d65998ae9c26797d2fce120ccd9f
caster <467906684@qq.com>
2017-11-03 16:53:00 +0800

    完成百度注册的检测

--- baidu_check.py
+++ baidu_check.py
@@ -5,6 +5,7 @@ import binascii
 import requests
 import time
 import math
+import hashlib
 from base64 import b64decode
 
 # agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
@@ -18,7 +19,6 @@ session = requests.session()
 
 
 def hex_md5(str):
-	import hashlib
 	return hashlib.md5(str).hexdigest()
 
 def get_gid():
@@ -33,7 +33,6 @@ def get_gid():
 		random_str=random_str+s
 	return random_str.upper()
 
-
 def get_callback():
     loopabc = '0123456789abcdefghijklmnopqrstuvwxyz'
     prefix = "bd__cbs__"
@@ -46,8 +45,8 @@ def get_callback():
     callback = prefix + ''.join(a)
     return callback
 
-
 def get_token():
+	global cookies
 	token_callback = get_callback()
 	#不能使用 params, 会自动排序参数,百度对参数的提交顺序有要求
 	token_url = "https://passport.baidu.com/v2/api/?getapi&tpl=mn&apiver=v3&tt="
@@ -56,16 +55,21 @@ def get_token():
 	token_url = token_url + token_callback
 
 	rsp = session.get(token_url, headers=headers , cookies=cookies)
-	token_content = rsp.content.replace(token_callback, "")
-	token_content = eval(token_content)
-	return token_content['data']['token']
+	cookies = rsp.cookies
+	try :
+		token_content = rsp.content.replace(token_callback, "")
+		return eval(token_content)['data']['token']
+	except Exception as e:
+		print(e)
+		print("response: "+rsp.content)
+		return ""
 
 def get_moonshad(account):
 	n = hex_md5(account+"Moonshadow")
-	n = n.replace("o", "ow").replace("d", "do").replace("a", "ad")
-	n = n.replace("h", "ha").replace("s", "sh").replace("n", "ns").replace("m", "mo")
+	#最多替换一次
+	n = n.replace("o", "ow", 1).replace("d", "do" ,1).replace("a", "ad" ,1)
+	n = n.replace("h", "ha" ,1).replace("s", "sh", 1).replace("n", "ns" ,1).replace("m", "mo" ,1)
 	return n
-
 
 def get_cookies():
 	#token 的获取必须带上cookies, 不然总是报错 "token":"the fisrt two args should be string type:0,1!"
@@ -78,19 +82,36 @@ cookies=get_cookies()
 gid=get_gid()
 token=get_token()
 
-
-
 def baidu_check(account):
-	print(token)
+	if token == "":
+		print("get_token null!!")
+		return -1
+	# print(token)
+	callback = get_callback()
+	moonshad = get_moonshad(account)
+	timenow = str(int(time.time()*1000))
+
 	regist_url = "https://passport.baidu.com/v2/?regphonecheck&token="
 	regist_url = regist_url + token +"&tpl=mn&apiver=v3&tt="
-	regist_url = regist_url + str(int(time.time()*1000)) + "&phone="
+	regist_url = regist_url + timenow + "&phone="
 	regist_url = regist_url + account + "&moonshad="
-	regist_url = regist_url + get_moonshad(account) + "&countrycode=&gid="
+	regist_url = regist_url + moonshad + "&countrycode=&gid="
 	regist_url = regist_url + gid + "&exchange=0&isexchangeable=1&action=reg&traceid=&callback="
-	regist_url = regist_url + get_callback()
+	regist_url = regist_url + callback
 
-	rsp = session.get(regist_url, headers=headers)
-	print(rsp.content)
+	# print(regist_url)
+	rsp = session.get(regist_url, headers=headers, cookies=cookies)
+	rsp = rsp.content.replace(callback, "")
+	rsp_json = eval(rsp)
+	try :
+		errno = rsp_json['errInfo']['no']
+		if errno=="0":	#未注册
+			return 0
+		elif errno=="130020":  #已注册
+			return 1
+	except Exception as e:
+		print(rsp_json)
+		print(e)
+	return -1
 
 
--- main.py
+++ main.py
@@ -15,15 +15,20 @@ sys.setdefaultencoding('utf8')
 import baidu_check
 from baidu_check import baidu_check
 
+def result_output(ret, site):
+	if ret == 1:
+		print("already regist " + site)
+	elif ret ==0:
+		print("not regist " + site)
+	else:
+		print(site + " check failed!")
 
 def regist_check(account):
-	if baidu_check(account)==0:
-		print("regist baidu!");
-	else:
-		print("not regist baidu")
+	result_output(baidu_check(account), "baidu")
+	# result_output(sina_check(account) , "sina")
 
 if __name__ == '__main__':
-	# account=raw_input(u'账号:')
-	account="13164757102"
+	account=raw_input(u'账号:')
+	# account="13164757102"
 	regist_check(account)
 
